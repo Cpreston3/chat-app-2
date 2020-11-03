cc9bd1bbe2c75e569878599a0c5add5d73127a2b
cpreston3 <caseypreston4@gmail.com>
2020-11-03 13:25:22 -0700

    updated the server and main js

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//run when client connects
+io.on('connect', socket => {
+
+    socket.emit('message','Welcome to Prestons\' Chat');
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A Preston has joined the chat');
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A Preston has left the chat');
+    });
+
+});
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
