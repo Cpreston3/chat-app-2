4c481d978e1e9711752b7b03b850aa89b1a87773
cpreston3 <caseypreston4@gmail.com>
2020-11-04 15:56:21 -0700

    updated the message box

--- public/script.js
+++ public/script.js
@@ -1,29 +1,41 @@
 const chatForm = document.getElementById('chat-form');
-
+const chatMessages = document.querySelector('.chat-messages');
 
 
 const socket = io();
 
-
+//Message from Server
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
+
+    //Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 //message Submit
 chatForm.addEventListener('submit', (e) =>{
     e.preventDefault();
 
+    //get message text
     const msg = e.target.elements.msg.value;
 
     console.log(msg);
-  //Emit message to user
+  //Emit message to server
     socket.emit('chatMessage', msg);
+
+    //clear input
+    e.target.elements.msg.value ='';
+    e.target.elements.msg.focus();
 });
 
 //Output message to Dom
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML`<p class="meta"></p>`
+    div.innerHTML =`<p class="meta">Casey <span>2:00</span></p>
+    <p class="text">
+    ${message}
+</p>`;
+document.querySelector('.chat-messages').appendChild(div);
 }
